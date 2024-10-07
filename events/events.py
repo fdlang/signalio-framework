67d3b57865f873fa67393d994c893fba346f54f7
Angel <purriaky@hotmail.com>
2024-10-08 01:40:29 +0200

    se crea clase SignalEvent

--- events/events.py
+++ events/events.py
@@ -9,15 +9,38 @@ class EventType(str, Enum): # hereda de str y Enum
     SIGNAL = "SIGNAL"
 
 
-class BaseEvent(BaseModel): # hereda de BaseModel
+class SignalType(str, Enum): 
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class OrderType(str, Enum):
+    MARKET = "MARKET"
+    LIMIT = "LIMIT"
+    STOP = "STOP"
+
+
+class BaseEvent(BaseModel): 
     event_type:EventType
 
     class Config:
         arbitrary_types_allowed = True
 
 
-class DataEvent(BaseEvent): # hereda de BaseEvent
+class DataEvent(BaseEvent): 
     event_type: EventType = EventType.DATA
     symbol:str
     data:pd.Series
+
+
+class SignalEvent(BaseEvent): 
+    event_type: EventType = EventType.SIGNAL
+    symbol: str
+    signal: SignalType
+    target_order: OrderType
+    target_price: float
+    order_id: int
+    sl: float # stop loss
+    tp: float # tipe profit
+
 
