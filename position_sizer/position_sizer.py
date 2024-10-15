e101f600df1b5891d0dd21afa941df8d5dd90b70
Angel <purriaky@hotmail.com>
2024-10-15 05:53:47 +0200

    modulo positionSizer terminado

--- position_sizer/position_sizer.py
+++ position_sizer/position_sizer.py
@@ -1,19 +1,38 @@
 from data_provider.data_provider import DataProvider
 from events.events import SignalEvent, SizingEvent
+from binance.client import Client
 from .interfaces.position_sizer_interface import IPositionSizer
+from .properties.position_sizer_properties import BaseSizerProps, MinSizingProps, FixedSizingProps, RiskPctSizingProps
+from .sizers.min_size_position_sizer import MinSizePositionSizer
+from .sizers.fixed_size_position_sizer import FixedSizePositionSizer
+from .sizers.risk_pct_position_sizer import RiskPctPositionSizer
 from queue import Queue
 
 
 class PositionSizer(IPositionSizer):
 
-    def __init__(self, events_queu: Queue, data_provider: DataProvider, sizing_properties):
+    def __init__(self, events_queu: Queue, data_provider: DataProvider, sizing_properties: BaseSizerProps):
         self.events_queue = events_queu
         self.DATA_PROVIDER = data_provider
         self.position_sizing_method = self._get_position_sizing_method(sizing_properties)
 
     
-    def _get_position_sizing_method(self, sizing_props) -> IPositionSizer:
-        pass
+    def _get_position_sizing_method(self, sizing_props: BaseSizerProps ) -> IPositionSizer:
+        """
+        Devuelve una instancia del position sizer apropiado en función del objeto de propiedades recibido.
+        """
+
+        if isinstance(sizing_props, MinSizingProps):
+            return MinSizePositionSizer()
+        
+        elif isinstance(sizing_props, FixedSizingProps):
+            return FixedSizePositionSizer(properties=sizing_props)
+        
+        elif isinstance(sizing_props, RiskPctSizingProps):
+            return RiskPctPositionSizer(properties=sizing_props)
+        
+        else:
+            raise(f"ERROR: Método de sizing desconocido: {sizing_props}")
 
 
     def __create_and_put_sizing_event(self, signal_event: SignalEvent, volume: float) -> None:
@@ -38,6 +57,9 @@ class PositionSizer(IPositionSizer):
         volume = self.position_sizing_method.size_signal(signal_event, self.DATA_PROVIDER)
 
         # Control de seguridad
+        if volume < Client().get_symbol_info(signal_event.symbol)['filters'][1]['minQty']:
+            print(f"ERROR: El volumen {volume} es menor al volumen mínimo admitido por el símbolo {signal_event.symbol}")
+            return None
 
         # Crea el evento y lo pone en la cola
-        self.__create_and_put_sizing_event(signal_event, volume)
+        self.__create_and_put_sizing_event(signal_event, volume) 
--- position_sizer/properties/position_sizer_properties.py
+++ position_sizer/properties/position_sizer_properties.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
 
 
+class BaseSizerProps(BaseModel):
+    pass
+
+
 class MinSizingProps(BaseModel):
     pass
 
