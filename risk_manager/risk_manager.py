501bd735f26f208517b96c55b22d0c986c31bf4a
Angel <purriaky@hotmail.com>
2024-10-29 08:42:57 +0100

    terminado risk_manager

--- risk_manager/risk_manager.py
+++ risk_manager/risk_manager.py
@@ -5,6 +5,7 @@ from data_provider.data_provider import DataProvider
 from portfolio.portfolio import Portfolio
 from events.events import SizingEvent, OrderEvent
 from binance.client import Client
+from utils.utils import Utils
 
 from queue import Queue
 
@@ -40,7 +41,7 @@ class RiskManager(IRiskManager):
         return total_value
     
 
-    def _compute_value_of_position_in_account_currency(self, symbol: str, volume: float) -> float:
+    def _compute_value_of_position_in_account_currency(self, symbol: str, volume: float, position_type: str) -> float:
 
         symbol_info = Client.get_symbol_info(symbol)
         min_qty = float(symbol_info['filters'][1]['minQty'])
@@ -48,15 +49,21 @@ class RiskManager(IRiskManager):
 
         if volume >= min_qty:
             traded_units = volume
+
+            # Valor de las unidades operadas en la divisa cotizada del simbolo
+            value_traded_in_profit_ccy = traded_units * self.DATA_PROVIDER.get_latest_tick(symbol)['bid']
+
+            # Valor de las unidades operadas en la divisa de la cuenta
+            value_traded_in_account_ccy = Utils.convert_currency_amount_to_another_currency(value_traded_in_profit_ccy, 
+                                                                                            symbol_info['quoteAsset'], 
+                                                                                            "USDT")
+            if position_type == Client().SIDE_SELL:
+                return -value_traded_in_account_ccy
+            else:
+                return value_traded_in_account_ccy
         else:
             print(f"ERROR (RiskManager): El volumen {volume} es inferior a las unidades permitidas {min_qty}")
         
-        return traded_units
-
-
-
-
-
 
     def _create_and_put_order_event(self, sizing_event: SizingEvent, volume: float) -> None:
 
@@ -74,6 +81,13 @@ class RiskManager(IRiskManager):
 
     
     def assess_order(self, sizing_event: SizingEvent) -> None:
+
+        # Obtiene el valor de todas las posiciones abiertas por la estrategia en la divisa de la cuenta.
+        current_position_value = self._compute_current_value_of_positions_in_account_currency()
+
+        # Obtiene el valor que tiene la nueva posición en la divisa de la cuenta
+        position_type = Client().SIDE_BUY if sizing_event.signal == "BUY" else Client().SIDE_SELL
+        new_position_value = self._compute_value_of_position_in_account_currency(sizing_event.symbol, sizing_event.volume, position_type)
         
         # Obtiene el nuevo volumen  de la operacio que se quiere ejecutar después de pasar por el risk manager.
         new_volume = self.risk_management_method.assess_order(sizing_event, current_position_value, new_position_value)
